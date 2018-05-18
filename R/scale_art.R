32060481d8632d73acff23e1c38da9e1801049af
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-05-18 08:29:33 -0700

    Starting to break out scale trans/breaks/labels

--- R/scale_art.R
+++ R/scale_art.R
@@ -14,22 +14,54 @@ scale_art <- function(collections = "nga_highlights", breaks_f = scales::log_bre
 }
 
 to_art <- function(x) {
-
+  closest_index <- ideal_indices(range(x), available_sets[["area"]])
+  attr(x, "breadbox_ci") <- closest_index
+  attr(x, "breadbox_src") <- "nga_highlights"
+  class(x) <- "breadbox"
+  return(x)
 }
 
-from_art <- function(x) as.numeric(x)
+from_art <- function(x) as.numeric(x) {
+  as.numeric(x)
+}
 
 art_trans <- function() {
   trans_new("art")
 }
 
+# Return a function that produces breaks
 art_breaks <- function(n = 3) {
   function(x) {
-    closest_index <- ideal_indices(range(x), available_sets[["area"]], n, ...)
-    closest_area <- available_sets[["area"]][i]
+    stopifnot(inherits(x, "breadbox"))
+    df <- get(attr(x, "breadbox_src"), envir = as.environment("package:breadbox"))
+    i <- attr(x, "breadbox_ci")
+    closest_area <- df[["area"]][i]
   }
 }
 
-f_art_labels <- function(i) {
-  closest_labels <- available_sets[["label"]][i]
+f_art_labels <- function(n = 3, i = NULL) {
+  function(x) {
+    stopifnot(inherits(x, "breadbox"))
+    if (is.null(i))
+      i <- ideal_indices(range(x), nga_highlights[["area"]], n, ...)
+    closest_labels <- available_sets[["label"]][i]
+  }
+}
+
+# ggplot2 scale functions ----
+
+# This tells ggplot2 what scale to look for, for yearmon
+scale_type.art <- function(x) "art"
+
+scale_art <- function(aesthetics, ...) {
+  ggplot2::continuous_scale(aesthetics, "art", identity,
+                   guide = "none", trans = art_trans(), ...)
+}
+
+scale_x_art <- function(...) {
+  scale_art(aesthetics = c("x", "xmin", "xmax", "xend"), ...)
+}
+
+scale_y_art <- function(...) {
+  scale_art(aesthetics = c("y", "ymin", "ymax", "yend"), ...)
 }
